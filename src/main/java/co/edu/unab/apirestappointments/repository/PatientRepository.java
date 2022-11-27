553fbda79c5468c36b833248c37e3ecc6aec5dd2
Ronald Mindiola <ronaldmindiola@live.com>
2022-11-26 22:17:31 -0500

    Fix

--- src/main/java/co/edu/unab/apirestappointments/repository/PatientRepository.java
+++ src/main/java/co/edu/unab/apirestappointments/repository/PatientRepository.java
@@ -3,9 +3,9 @@ package co.edu.unab.apirestappointments.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import co.edu.unab.apirestappointments.models.PatientModel;
+import co.edu.unab.apirestappointments.models.Patient;
 
 @Repository
-public interface PatientRepository extends MongoRepository<PatientModel, String> {
+public interface PatientRepository extends MongoRepository<Patient, String> {
     
 }
