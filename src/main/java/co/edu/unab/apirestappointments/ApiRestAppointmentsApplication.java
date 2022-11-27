9716b21a6dd187dbe1b6e80960ca5cf4d9a84c46
Ronald Mindiola <ronaldmindiola@live.com>
2022-11-26 20:17:47 -0500

    Init

---
+++ src/main/java/co/edu/unab/apirestappointments/ApiRestAppointmentsApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unab.apirestappointments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestAppointmentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestAppointmentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/unab/apirestappointments/ApiRestAppointmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unab.apirestappointments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestAppointmentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
