89271244952328fc53789dcba635a6499f1f94f7
Ronald Mindiola <ronaldmindiola@live.com>
2022-11-27 12:09:17 -0500

    api 1.0

--- src/main/java/co/edu/unab/apirestappointments/repository/PatientRepository.java
+++ src/main/java/co/edu/unab/apirestappointments/repository/PatientRepository.java
@@ -2,7 +2,6 @@ package co.edu.unab.apirestappointments.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
-
 import co.edu.unab.apirestappointments.models.Patient;
 
 @Repository
---
+++ src/main/java/co/edu/unab/apirestappointments/services/PatientService.java
@@ -0,0 +1,9 @@
+package co.edu.unab.apirestappointments.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PatientService {
+    
+    
+}
