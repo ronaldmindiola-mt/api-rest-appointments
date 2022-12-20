c8445560eeca93cf9e6d5ef586b1226a331fd314
Ronald Mindiola <ronald.mindiola.mt@usa.edu.co>
2022-12-20 18:20:51 -0500

    2

--- src/main/java/co/edu/unab/apirestappointments/repository/AgendaRepository.java
+++ src/main/java/co/edu/unab/apirestappointments/repository/AgendaRepository.java
@@ -1,10 +1,7 @@
 package co.edu.unab.apirestappointments.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-
 import co.edu.unab.apirestappointments.models.Agenda;
-
-
 public interface AgendaRepository extends MongoRepository<Agenda, String> {
 
 }
